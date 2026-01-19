e3edc46eff4d800884509b21ac1e854c9c8ebd23
Szymon <szymek.fogiel@gmail.com>
2026-01-19 15:03:40 +0100

    Timeout and better defaults.

--- keyboards/keychron/q5_he/ansi_encoder/keymaps/mine/config.h
+++ keyboards/keychron/q5_he/ansi_encoder/keymaps/mine/config.h
@@ -2,3 +2,7 @@
 
 // Expand the number of layers to 8
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_DEFAULT_HUE 156
+#define RGB_MATRIX_DEFAULT_SAT 191
+#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
