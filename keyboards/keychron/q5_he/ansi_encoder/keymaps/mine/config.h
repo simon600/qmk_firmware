dae251a08f52012c97f97be1e2b5bcc936e59842
Szymon <szymek.fogiel@gmail.com>
2026-01-19 20:55:39 +0100

    Simplified with buffer.

--- keyboards/keychron/q5_he/ansi_encoder/keymaps/mine/config.h
+++ keyboards/keychron/q5_he/ansi_encoder/keymaps/mine/config.h
@@ -6,3 +6,6 @@
 #define RGB_MATRIX_DEFAULT_HUE 156
 #define RGB_MATRIX_DEFAULT_SAT 191
 #define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
+
+// Use SignalRGB buffer mode for better control of LED application order
+#define SIGNALRGB_USE_BUFFER
